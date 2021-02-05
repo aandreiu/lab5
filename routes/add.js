dd09ce44f2de5403f959e3986095167fa99f6439
Alexandra Andreiu <aandreiu@ucsd.edu>
2021-02-04 17:06:19 -0800

    updated HCI Friends

--- routes/add.js
+++ routes/add.js
@@ -3,5 +3,5 @@ var data = require("../data.json");
 exports.addFriend = function(request, response) {  
 	 response.render('index', data.newFriend)
 	 //data.friends.push(newFriend)
-	 // this is where I got stuck with the lab (my text editor also keeps giving me weird syntax errors)
+	 // this is where I got stuck with the lab
  };
